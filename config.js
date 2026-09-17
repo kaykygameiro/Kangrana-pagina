6096fc6f1187dddb4797df6cdd529e24ce7d02f2
kaykygameiro <kayky.22204708360060@faeterj-rio.edu.br>
2026-09-17 01:19:36 -0300

    update APP_URL to valid HTTPS link for deployment

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // APP_URL is optional; the Entrar link appears only when it is a valid HTTPS URL.
 window.KANGRANA_CONFIG = Object.freeze({
   CHECKOUT_URL: 'https://pay.kiwify.com.br/iH9r0HQ',
-  APP_URL: '',
+  APP_URL: 'https://kangrana.vercel.app/',
   BILLING_DESCRIPTION: 'Pagamento único'
 });
